4d9af269a61aeca0a46b00915b137894007864ff
sagar <sagart12@gmail.com>
2016-10-16 13:11:42 -0400

    First Commit

---
+++ app/src/main/java/com/sagar/panther/Utils/MyFirebaseInstanceIDService.java
@@ -0,0 +1,34 @@
+package com.sagar.panther.Utils;
+
+
+import android.util.Log;
+
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+/**
+ * Created by Sagar on 10/14/2016.
+ */
+
+
+//Class extending FirebaseInstanceIdService
+public class MyFirebaseInstanceIDService extends FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+
+        //Displaying token on logcat
+        Log.d(TAG, "Refreshed token: " + refreshedToken);
+
+    }
+
+    private void sendRegistrationToServer(String token) {
+        //You can implement this method to store the token on your server
+        //Not required for current project
+    }
+}
